aad6b81d0c80fe432b2bc29c3a8500bd135c5b7b
Tilav <113688202+Tilav-232007-P@users.noreply.github.com>
2022-09-30 20:06:39 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let me = "tilav"
+me = "Tilav"
+console.log(me); //мини задание
+let my_name = "TILAV"
+console.log(typeof my_name); //string
+let age = 15
+console.log(typeof age); //number
+let user = { name: "TilaV"}
+console.log(typeof user); //object
+//Типы данных которые возникают при ошибке
+let people;
+console.log(typeof people); //undefined
+let lion = null;
+console.log(lion); //null
+let dig = 1123123123133312312312323447584758897583475n
+console.log(typeof dig); //bigint
+
+
+
+
